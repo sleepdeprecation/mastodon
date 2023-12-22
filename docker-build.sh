7b7ad6aefedaf0c66fbcbaef8a1dca434017bfac
Don Kuntz <don@kuntz.co>
2023-12-22 10:49:15 -0600

    add new streaming container

--- docker-build.sh
+++ docker-build.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-set +x +e
+set -x -e
 
 TODAY=$(date +%Y-%m-%d)
 REPO="ghcr.io/sleepdeprecation/mastodon"
+REPO_STREAMING="ghcr.io/sleepdeprecation/mastodon-streaming"
 
 docker buildx build -t "${REPO}:${TODAY}" ./
-
 docker tag "${REPO}:${TODAY}" "${REPO}:latest"
 
+docker buildx build -t "${REPO_STREAMING}:${TODAY}" -f streaming/Dockerfile .
+docker tag "${REPO_STREAMING}:${TODAY}" "${REPO_STREAMING}:latest"
+
 docker push --all-tags "${REPO}"
+docker push --all-tags "${REPO_STREAMING}"
